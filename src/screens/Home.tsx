fd74220c4a363f9668142fe32b35859fa998c7c8
Guilherme Cursino <guiiix93@gmail.com>
2024-09-10 20:43:22 -0300

    Exercise page started

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,48 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { VStack, HStack, Heading, Text } from "@gluestack-ui/themed";
+
+import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { VStack } from "@gluestack-ui/themed";
+import { ExerciseCard } from "@components/ExerciseCard";
 
 export function Home(){
+  const [groups, setGroups] = useState(["Costas", "Bíceps", "Tríceps", "Ombro"])
+  const [groupSelected, setGroupSelected] = useState("Costas")
+
   return (
     <VStack flex={1} >
       <HomeHeader />
+
+      <FlatList 
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({item}) => (
+          <Group 
+          name={item}
+          isActive={groupSelected === item} 
+          onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingHorizontal: 32}}
+        style={{ marginVertical: 40, maxHeight: 44, minWidth: 44}}
+      />
+
+      <VStack px="$8">
+        <HStack justifyContent="space-between" mb="$5" alignItems="center">
+          <Heading color="$gray200" fontSize="$md" fontFamily="$heading">
+            Exercícios
+          </Heading>
+
+          <Text color="$gray200" fontSize="$sm" fontFamily="$body">
+            4
+          </Text>
+        </HStack>
+
+        <ExerciseCard />
+      </VStack>
     </VStack>
   );
 }
